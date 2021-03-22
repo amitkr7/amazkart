e5d4f735afeab9f5a707d63ede05bfee774e3332
Amit <amit840901@gmail.com>
2021-03-22 22:47:18 +0530

    Order Screen Page and functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import RegisterScreen from './screens/RegisterScreen'
 import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
+import OrderScreen from './screens/OrderScreen'
 
 function App() {
   const cart = useSelector((state) => state.cart)
@@ -63,6 +64,7 @@ function App() {
           <Route path='/shipping' component={ShippingScreen}></Route>
           <Route path='/payment' component={PaymentScreen}></Route>
           <Route path='/placeorder' component={PlaceOrderScreen}></Route>
+          <Route path='/order/:id' component={OrderScreen}></Route>
           <Route path='/' component={HomeScreen} exact />
         </main>
         <footer className='row center'>All Rights Reserved &copy;</footer>
